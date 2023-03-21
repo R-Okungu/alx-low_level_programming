154aa45fac9c3180a366ed7214fdadb2866376d3
R-Okungu <okunguronney@gmail.com>
2023-03-21 08:43:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,21 +2,17 @@
 
 /**
  * print_alphabet_x10 - print alphabet x10
- * Return: always 0
  */
 
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0;
+	int i;
 
-	while (i <= 9)
-	{
-	for (c = 'a'; c <= 'z'; c++)
-		}
-			_putchar(c);
-		}
-	_putchar('\n');
-	i++;
+	for (i = 1; i <= 10; i++)
+	}
+		for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
+		_putchar('\n');
 	}
 }
